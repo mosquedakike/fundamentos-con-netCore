0f8a21575874d82c5196f3b34db7e7249895df14
mosquedakike <enrique_inf@hoptmail.com>
2019-09-26 17:14:49 -0500

    Agregar archivos de proyecto.

---
+++ netCore/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace netCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CALCULADORA
+
+            int suma;
+            int resta;
+            int multiplicacion;
+            int division;
+            int option;
+
+            Console.WriteLine("inserte el valor de a: ");
+            int a = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Inserte el valor de b: ");
+            int b = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Selecciona alguna de las siguientes opciones:\n\nSumar: 1\nRestar: 2\nMultiplicar: 3\nDividir: 4\n");
+            option = Convert.ToInt16(Console.ReadLine());
+
+
+            if (option == 1)
+            {
+                suma = a + b;
+                Console.WriteLine("El resultado de a + b es: " + suma);
+            }
+            if (option == 2)
+            {
+                resta = a - b;
+                Console.WriteLine("El resultado de a - b es: " + resta);
+            }
+            if (option == 3)
+            {
+                multiplicacion = a * b;
+                Console.WriteLine("El resultado de a * b es: " + multiplicacion);
+            }
+            if (option == 4)
+            {
+                division = a / b;
+                Console.WriteLine("El resultado de a / b es: " + division);
+            }
+
+            Console.ReadLine();
+
+            //********************************************************************************************************************************************
+
+            /*
+             * TIPOS DE OPERADORES:
+             *  < menor que 
+             *  > mayor que
+             *  <= menor o igual que
+             *  >= mayor o igual que
+             *  == igual a
+             *  ! diferente de
+             *  != diferente o igual a
+             *  && y
+             *  || o
+             
+
+            int valor1 = 9;
+            int valor2 = 8;
+            int valor3 = 10;
+            int valor4 = 10;
+            var data = valor1 != valor2;
+
+            if (valor1!=valor2 && valor3==valor4)
+            {
+                if (valor1<valor4)
+                {
+                    Console.WriteLine("Entrando al segundo if");
+                    Console.WriteLine("El valor de DATA es: true ");
+                }
+                else
+                {
+                    Console.WriteLine("Entrando al else anidado");
+                }           
+            }
+            else
+            {
+                Console.WriteLine("El valor de DATA es: false");
+            }
+
+            Console.ReadLine();
+            */
+        }
+    }
+}
